d96c8005cb7813670250f732d1874326c7701f14
bindusarika <bindusarika333@gmail.com>
2023-10-24 16:01:45 +0530

    carwebsite files added

---
+++ Digital_nest_projects/webserver.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+yum install httpd git -y
+
+service httpd start
+
+chkconfig httpd on
+
+cd /var/www/html
+
+git clone https://github.com/sreesysadm/carwebsite.git .
+
+service httpd restart
+
